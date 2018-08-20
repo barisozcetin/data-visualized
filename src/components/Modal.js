7924d44b1028b33ff78a2076c627c7efda08138e
baris_000 <barisozcetin@gmail.com>
2018-08-19 20:27:25 -0700

    modal

---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Modal = (props) => {
+	return (
+		<div className={`modal ${props.isActive && 'is-active'}`}>
+			<div className="modal-background" onClick={props.onClose} />
+			<div className="modal-content">{props.children}</div>
+			<button className="modal-close is-large" aria-label="close" onClick={props.onClose} />
+			<style jsx>{`
+				.modal-content {
+					background-color: white;
+					min-height: 80vh;
+					min-width: 50vw;
+					margin: 15px;
+					display: flex;
+					justify-content: center;
+				}
+			`}</style>
+		</div>
+	);
+};
+
+export default Modal;
